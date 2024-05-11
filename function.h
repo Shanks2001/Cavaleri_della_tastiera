73b538430c0863cd4924540cdd325dce128e79a6
CringheLord <v.grottola1@studenti.uniba.it>
2024-05-11 14:03:34 +0200

    Create function.h

---
+++ function.h
@@ -0,0 +1,35 @@
+#ifndef FUNCTION_H
+//LVL_P --> puntatore Universale LVL Attuale
+//func_arrayn per chiamare un (n) livello
+
+
+
+
+void (*func_array[10])() = { lvl_0, lvl_1, lvl_2, lvl_3, lvl_4, lvl_5, lvl_6, lvl_7, lvl_8, lvl_9 }; 
+
+
+void lvl_save ( LVL_P )
+{
+    
+    Actual_lvl = *LVL_P;
+    
+    fp = fopen ( saveLvl.txt, "w+" ); 
+    
+    if ( fp == NULL )
+    {
+        perror ( "data.txt" );
+        exit (1);
+    }
+    
+    fputs ( "%d", Actual_lvl );
+    
+    
+}
+
+
+
+
+
+
+
+#endif // FUNCTION_H
