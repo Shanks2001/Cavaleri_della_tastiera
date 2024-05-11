eea9cf02a9594e7c1b6fd9e4cde9c188af751d94
CringheLord <v.grottola1@studenti.uniba.it>
2024-05-11 14:36:51 +0200

    Update function.h

--- function.h
+++ function.h
@@ -1,11 +1,17 @@
 #ifndef FUNCTION_H
 //LVL_P --> puntatore Universale LVL Attuale
 //func_arrayn per chiamare un (n) livello
+/////////////////////////////////////////////////
+void W_checkpoint( int *live );
 
+void R_checkpoint( int *live );
 
+void lvl_save ( LVL_P );
 
+//////////////////////////////////////////////////
 
 void (*func_array[10])() = { lvl_0, lvl_1, lvl_2, lvl_3, lvl_4, lvl_5, lvl_6, lvl_7, lvl_8, lvl_9 }; 
+
 
 
 void lvl_save ( LVL_P )
@@ -13,11 +19,11 @@ void lvl_save ( LVL_P )
     
     Actual_lvl = *LVL_P;
     
-    fp = fopen ( saveLvl.txt, "w+" ); 
+    FILE* fp = fopen ( saveLvl.txt, "w+" ); 
     
     if ( fp == NULL )
     {
-        perror ( "data.txt" );
+        perror ( "saveLvl.txt" );
         exit (1);
     }
     
@@ -27,31 +33,34 @@ void lvl_save ( LVL_P )
 
 
 lvl_
+//////////////////////////////////////////////////////////
 
-void cekpoin(int *live);
-void readckpoin(int *live);
 
-int main(){
-    int live =3; 
-    readckpoin(&live);
-    cekpoin(&live);
-    printf("%d", live);
-    return 0;
+
+void W_checkpoint( int *live )
+{
+ FILE* fp = fopen ("life.txt", "w+" );
+    if ( fp == NULL )
+    {
+        perror ( "life.txt" );
+        exit (1);
+    }
+    
+ fprintf ( fp, "%d", live );
+ fclose ( fp );
 }
 
-void cekpoin(int *live){
- FILE *fp=fopen("life.txt", "w+");
-        if(fp==NULL)
-    return;
- fprintf(fp, "%d", *live);
- fclose(fp);
+void R_checkpoint ( int *live )
+{
+    FILE* fp = fopen( "life.txt", "r+" );
+    if ( fp == NULL )
+    {
+        perror ( "life.txt" );
+        exit (1);
+    }
+    fscanf( fp, "%d", life );
+    fclose( fp );
 }
 
-void readckpoin(int *live){
-    FILE *fp2=fopen("life.txt", "r+");
-        if(fp2==NULL)
-         return;
-    fscanf(fp2,"%d", live);
-    fclose(fp2);
-}
+/////////////////////////////////////
 #endif // FUNCTION_H
