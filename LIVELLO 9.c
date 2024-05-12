898da339db38a80fc9ac21d433acf9588e91c5d0
Shanks <107371248+Shanks2001@users.noreply.github.com>
2024-05-12 11:11:30 +0200

    Update LIVELLO 9.c
    giovanni

--- LIVELLO 9.c
+++ LIVELLO 9.c
@@ -1,6 +1,4 @@
-
-void lvl_8(char *Scelta, int *LIFE)
-{
+void livello_9(int *LIFE){
     char vero[]= "i++";
     char vero1[] = "&";
     char vero2[] = ";";
@@ -9,11 +7,11 @@ void lvl_8(char *Scelta, int *LIFE)
     char vero5[]= "fclose(file)";
     char vero6[] = "for";
     char vero7[]= "i";
-    
-    printf("~ ~ LIVELLO 8 ~ ~")
-    printf("scrivi cosa manca\n"); //i++,%d,;
+    char Scelta[max];
 
-    printf("void aggiungi(Clienti *c, int *f){\nif(*f<50){\nint b=0;\ndo {\nprintf(\"scrivi quante prenotazioni vuoi aggiungere:\");\nscanf(\"%%d\", &b);\n}while(b<=0);\nfor(int i=0; i<b; ){\nprintf(\"scrivi il nome:\");\nscanf(\"%%99s\",  c[*f].nome);\nprintf(\"scrivi il numero di persone:\");\nscanf(\"\", &c[*f].npersone);\nprintf(\"scrivi il numero del tavolo:\")\nscanf(\"%%d\", &c[*f].tavolo);\nprintf(\"scrivi l'ora(ora):\");\nscanf(\"%%d\", &c[*f].data.ora);\nprintf(\"scrivi l'ora(minuti):\");\nscanf(\"%%d\", &c[*f].data.minuti);\n(*f)++;\n}\n}else{\nprintf(\"ristorante pieno\");\n}\nkill();\n}\n");
+    printf("scrivi cosa manca\n");
+   //i++,&,;
+    printf("void aggiungi(Clienti *c, int *f){\nif(*f<50){\nint b=0;\ndo {\nprintf(\"scrivi quante prenotazioni vuoi aggiungere:\");\nscanf(\"%%d\", &b);\n}while(b<=0);\nfor(int i=0; i<b; ){\nprintf(\"scrivi il nome:\");\nscanf(\"%%99s\",  c[*f].nome);\nprintf(\"scrivi il numero di persone:\");\nscanf(\"%%d\", c[*f].npersone);\nprintf(\"scrivi il numero del tavolo:\")\nscanf(\"%%d\", &c[*f].tavolo);\nprintf(\"scrivi l'ora(ora):\");\nscanf(\"%%d\", &c[*f].data.ora);\nprintf(\"scrivi l'ora(minuti):\");\nscanf(\"%%d\", &c[*f].data.minuti);\n(*f)++;\n}\n}else{\nprintf(\"ristorante pieno\");\n}\nkill();\n}\n");
     do
     {
         printf("1:");
@@ -88,7 +86,7 @@ void lvl_8(char *Scelta, int *LIFE)
     }while(strcmp(Scelta, vero2)!=0);
 
     memset(Scelta, '\0', max); //RESET ARRAY
-    
+
     printf("scrivi cosa manca \n");//i++,if,i,;
     printf("void stampa(Clienti *c, int f){\nfor(int i=0; i<f; ){\nprintf(\"%%d) nome:%%s numero persone:%%d numero del tavolo:%%d orario:%%d:%%d\", \ni+1,c[i].nome, c[i].npersone, c[i].tavolo, c[i].data.ora, c[i].data.minuti);\n}\n}\nvoid ricerca( Clienti *c, int f){\nint MAX = 0;\nint j = 0;\nfor(int i=0; i < f; i++){\n\n(MAX < c[].npersone){\nMAX=c[].npersone;\nj=i;\n}\n}\ntroll();\nprintf(\"nome:%%s numero di persone:%%d numero del tavolo:%%d\", c[j].nome, c[j].npersone, c[j].tavolo);\n}\n");
     do
@@ -166,8 +164,9 @@ void lvl_8(char *Scelta, int *LIFE)
 
     memset(Scelta, '\0', max); //RESET ARRAY
 
-    printf("scrivi cosa manca\n");//for,*file,fclose(file);
+    printf("scrivi cosa manca\n");//for,*file,fclose(file),;
     printf("(int i = 0; i < 10; ){\nvoid ordinamento(Clienti *c, int f){\n\nfor(int i=0; i<f-1; i++){\n(int j=0; j<f-i-1; j++){\nif(c[j].data.ora < c[j+1].data.ora){\nClienti max=c[j+1];\nc[j+1]=c[j];\nc[j]=max;\n}\nif(c[j].data.ora == c[j+1].data.ora && c[j].data.minuti < c[j+1].data.minuti){\nClienti max=c[j+1];\nc[j+1]=c[j];\nc[j]=max;\n}\n}\n\n}\n}\nvoid file(Clienti *c, int f){\n\nFILE =fopen(\"prenotazioni.txt\", \"w\");\nif(file==NULL){\nprintf(\"ERROR\");\n}else{\nprintf(\"file aperto con succeso\n\");\n}\n\nfprintf(file, \"numero prenotazioni %d\", f);\nfor(int i=0; i<f; i++){\n\nfprintf(file, \"%d) nome:%s numero persone:%d numero tavolo:%d orario:%d:%d\", i+1, c[i].nome, c[i].npersone, c[i].tavolo, c[i].data.ora, c[i].data.minuti);\n}\nstampa_fregato_in_loop();\n\nprintf(\"\nfile salvato\");\n}\n");
+    printf("1:");
     do
     {
         printf("1:");
@@ -271,6 +270,4 @@ void lvl_8(char *Scelta, int *LIFE)
 
     memset(Scelta, '\0', max); //RESET ARRAY
     printf("BOSS");
-
-    return 0;
 }
